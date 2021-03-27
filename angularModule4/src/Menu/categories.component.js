2a54dec5c544cdef766e93177695844e8d3671b4
somesh1611 <55554219+somesh1611@users.noreply.github.com>
2021-03-28 00:42:47 +0530

    Update categories.component.js

--- angularModule4/src/Menu/categories.component.js
+++ angularModule4/src/Menu/categories.component.js
@@ -1,1 +1,12 @@
-
+(function () {
+    'use strict';
+    
+    angular.module('MenuApp')
+    .component('categories', {
+      templateUrl: 'src/Menu/templates/categories.template.html',
+      bindings: {
+        categories: '<'
+      }
+    });
+    
+    })();
